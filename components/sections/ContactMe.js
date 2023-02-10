7da2b1711c2b9d8459ac74c761f2f235cbf63f60
okanay <okanay@hotmail.com>
2023-02-10 15:02:13 +0300

    alpha10

--- components/sections/ContactMe.js
+++ components/sections/ContactMe.js
@@ -22,7 +22,7 @@ const ContactMe = () => {
                  name="name"
                  id="name"
           />
-          <label htmlFor={'name'} className={'absolute left-0 -top-1.5 text-gray-400 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 transition-all duration-150 pointer-events-none'}>Adiniz</label>
+          <label htmlFor={'name'} className={'absolute left-0 -top-1.5 text-gray-400 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 transition-all duration-150 pointer-events-none'}>Adınız</label>
         </div>
 
         <div className={'relative py-2'}>
@@ -42,7 +42,7 @@ const ContactMe = () => {
                     placeholder={"Fikirlerinizi bizimle belirtebilirsiniz.."}
                     name="message"
           />
-          <label htmlFor={'message'} className={'absolute left-0 -top-1.5 text-gray-400 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 transition-all duration-150 pointer-events-none'}>Dusuncelerinizi yazabilirsiniz..</label>
+          <label htmlFor={'message'} className={'absolute left-0 -top-1.5 text-gray-400 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 transition-all duration-150 pointer-events-none'}>Düşüncelerinizi Buraya Yazabilirsiniz..</label>
         </div>
           <div className={'flex flex-col-4 justify-end items-end'}>
             <button type={"submit"} className={'py-2 px-6 bg-blue-400 border border-gray-400/20 rounded-md text-center text-slate-50 transition duration-300 hover:text-slate-50 hover:bg-slate-500'}>Gönder</button>
