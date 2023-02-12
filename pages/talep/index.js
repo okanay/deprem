b61c6a36db9b4e4ae671415719a906815ff44776
okanay <okanay@hotmail.com>
2023-02-13 00:07:49 +0300

    alpha45

--- pages/talep/index.js
+++ pages/talep/index.js
@@ -74,17 +74,32 @@ const Talep = () => {
       opacity: 0,
       scaleY: 0,
       y: -10,
-      transition: { duration: 0.25 },
+      transition: { duration: 0.2 },
     },
     open: {
       opacity: 1,
       scaleY: 1,
-      y: 0,
+      y: -2,
       display: "block",
       scaleX: 0.95,
       transition: { duration: 0.5, type: "spring" },
     },
   };
+
+  const imageContainer = {
+    initial: {},
+    unselected: {
+      y: 0,
+      scale : 1,
+      transition: { duration: 0.3 },
+    },
+    selected: {
+      y: -4,
+      scale : 1.15,
+      transition: { duration: 0.5, type: "spring" },
+    },
+  };
+
   const handleFilterForm = (item) => {
     const selectedValue = { color: item.color, url: item.url };
     setCreateButton(selectedValue);
@@ -93,7 +108,11 @@ const Talep = () => {
   };
 
   return (
-    <div className={"bg-gray-50 max-w-screen-phoneXS phoneLG:max-w-screen-phoneLG phone:max-w-screen-phone w-full m-20 my-8 mx-auto px-4 py-8"}>
+    <div
+      className={
+        "bg-gray-50 max-w-screen-phoneXS phoneLG:max-w-screen-phoneLG phone:max-w-screen-phone w-full m-20 my-8 mx-auto px-4 py-8"
+      }
+    >
       {/*Uyari*/}
 
       <Alert title={"Lütfen Dikkat!"}>
@@ -123,55 +142,81 @@ const Talep = () => {
         </Link>
       </div>
 
-      {/*  Talep Iconlari olsun ve o iconlara gore filtreleme olsun.. baslayalim..*/}
-      <div className={"flex flex-row justify-center gap-4 bg-gray-50 w-full pb-7 pt-2 border-b-2 border-gray-800 rounded-[2px]"}>
+      {/*  FILTER*/}
+      <div
+        className={
+          "flex flex-row justify-center gap-4 bg-gray-50 w-full pb-7 pt-2 border-b-2 border-gray-800 rounded-[2px]"
+        }
+      >
         {talepFilter.map((item) => {
           return (
-            <button
-              key={item.name}
-              onClick={() => {
-                handleFilterForm(item);
-              }}
-              className={`w-10 h-10 p-2 border-2 transition duration-300 rounded flex flex-col items-center group ${
-                selected === item.name
-                  ? `border-${item.color} border-b-[4px]`
-                  : ""
-              } relative`}
-            >
-              <Image
-                src={item.src}
-                alt={item.alt}
-                width={"200"}
-                height={"200"}
-              />
+            <div className={'relative flex flex-col items-center text-center group'}>
+              <m.div
+                key={item.name}
+                variants={imageContainer}
+                initial={"unselected"}
+                animate={selected === item.name ? "selected" : "unselected"}
+              >
+                <button
+                  onClick={() => {
+                    handleFilterForm(item);
+                  }}
+                  className={`w-10 h-10 p-2 border-2 transition duration-300 rounded group ${
+                    selected === item.name
+                      ? `border-${item.color} border-b-[4px]`
+                      : ""
+                  } `}
+                >
+                  <Image
+                    src={item.src}
+                    alt={item.alt}
+                    width={"200"}
+                    height={"200"}
+                  />
+                </button>
+              </m.div>
 
               <m.label
                 variants={labelContainer}
                 initial={"hidden"}
                 animate={selected === item.name ? "open" : "hidden"}
                 htmlFor={item.name}
-                className={`absolute top-10 text-gray-800 font-light border-b-2 pb-0.5 w-[4rem] border-${item.color} ${item.hover}`}
+                className={`absolute top-10 text-gray-800 font-light border-b-2 mt-1.5 w-[4rem] border-${item.color} ${item.hover}`}
               >
                 {item.description}
               </m.label>
-            </button>
+            </div>
           );
         })}
       </div>
 
       {/*  TAILWIND CLASS CREATOR*/}
       <div>
-
         {/* BackGround*/}
-        <div className={'hover:bg-slate-600 hover:bg-red-400 hover:bg-teal-400 hover:bg-pink-400 hover:bg-blue-400 hover:bg-emerald-400'}></div>
+        <div
+          className={
+            "hover:bg-slate-600 hover:bg-red-400 hover:bg-teal-400 hover:bg-pink-400 hover:bg-blue-400 hover:bg-emerald-400"
+          }
+        ></div>
         {/* TEXT */}
-        <div className={'text-red-400 text-teal-400 text-pink-400 text-blue-400 text-emerald-400 text-slate-600'}></div>
+        <div
+          className={
+            "text-red-400 text-teal-400 text-pink-400 text-blue-400 text-emerald-400 text-slate-600"
+          }
+        ></div>
         {/* BORDER*/}
-        <div className={'border-slate-600 border-red-400 border-teal-400 border-pink-400 border-blue-400 border-emerald-400'}></div>
-        <div className={'hover:border-slate-400 hover:border-red-300 hover:border-teal-300 hover:border-pink-300 hover:border-blue-300 hover:border-emerald-300'}></div>
+        <div
+          className={
+            "border-slate-600 border-red-400 border-teal-400 border-pink-400 border-blue-400 border-emerald-400"
+          }
+        ></div>
+        <div
+          className={
+            "hover:border-slate-400 hover:border-red-300 hover:border-teal-300 hover:border-pink-300 hover:border-blue-300 hover:border-emerald-300"
+          }
+        ></div>
       </div>
     </div>
   );
 };
-
 export default Talep;
