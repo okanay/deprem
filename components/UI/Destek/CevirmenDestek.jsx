15f434dae34de292f69e04e3242ad85f577df534
okanay <okanay@hotmail.com>
2023-02-23 01:34:14 +0300

    V0-B10.40

--- components/UI/Destek/CevirmenDestek.jsx
+++ components/UI/Destek/CevirmenDestek.jsx
@@ -2,9 +2,6 @@ import * as Yup from "yup";
 import React, { useEffect, useState } from "react";
 import { emergencyCity, TurkeyCity } from "../../../helper/getCityAndStreet";
 
-
-
-
 const Info1Data = TurkeyCity;
 const Info2Data = TurkeyCity;
 const CevirmenDestek = ({
@@ -12,6 +9,8 @@ const CevirmenDestek = ({
   destekSchema,
   setValidationSchema,
 }) => {
+
+  const [hiddenMenu, setHiddenMenu] = useState(false)
   const [selectedInfo1, setSelectedInfo1] = useState({
     key: "I1_DATA_0",
     value: "",
@@ -80,23 +79,54 @@ const CevirmenDestek = ({
 
   return (
     <div className={"flex flex-col text-sm mt-2 mb-4"}>
-      <div className={"flex flex-col justify-between items-start"}>
-        <div
+      <div className={"flex flex-col gap-2 justify-between items-start"}>
+        <button
+          type={'button'}
+          onClick={() => {setHiddenMenu(!hiddenMenu)}}
           className={
-            "bg-blue-400/80  w-full h-11 rounded shadow-sm shadow-neutral-800/20"
+            "border-l-2 border-r-2 border-blue-400 bg-neutral-50 w-full h-11 shadow-sm shadow-neutral-800/20"
           }
         >
           <div className={"flex flex-row justify-between items-center h-full"}>
             <div className="flex flex-col px-2">
-              <p className={"text-lg text-neutral-50"}>Dil Seçin</p>
+              <p className={"text-lg text-neutral-700"}>Dil Seçin</p>
             </div>
             <div className="flex flex-col px-2">
-              <div className={'w-4 h-4'}>
+              <p className={"text-lg text-neutral-700"}>|</p>
+            </div>
+          </div>
+        </button>
 
+        {hiddenMenu && (
+          <div className={"flex flex-col bg-neutral-50 w-full shadow-sm shadow-neutral-800 h-24 overflow-y-scroll scrollbar-hide"}>
+            <div className="flex items-center">
+              <div className={"border-l-2 border-r-2 border-slate-500 h-8 w-full flex flex-col justify-center px-2 hover:border-emerald-400"}>
+                <p>Fransizca</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className={"border-l-2 border-r-2 border-slate-500 h-8 w-full flex flex-col justify-center px-2 hover:border-emerald-400"}>
+                <p>Ingılızce</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className={"border-l-2 border-r-2 border-slate-500 h-8 w-full flex flex-col justify-center px-2 hover:border-emerald-400"}>
+                <p>Arapca</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className={"border-l-2 border-r-2 border-slate-500 h-8 w-full flex flex-col justify-center px-2 hover:border-emerald-400"}>
+                <p>Ispanyolca</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className={"border-l-2 border-r-2 border-slate-500 h-8 w-full flex flex-col justify-center px-2 hover:border-emerald-400"}>
+                <p>Almanca</p>
               </div>
             </div>
           </div>
-        </div>
+        )}
+
       </div>
     </div>
   );
