5be922db2baafba6916421e21654d4fa56212fa0
okanay <okanay@hotmail.com>
2023-03-02 07:06:28 +0300

    V2.05GoogleAnalytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "react-query";
+import Script from "next/script";
 
 // import { Provider } from "react-redux";
 // import store from "../src/app/store";
@@ -16,6 +17,22 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Script
+        strategy={"afterInteractive"}
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-3FD9YLP5HK"
+      ></Script>
+      <Script strategy={"afterInteractive"} id={"google-analytics"}>
+        {`
+        
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3FD9YLP5HK');
+        `}
+      </Script>
+
       <div className={"scroll-smooth"}>
         <Layout>
           <Component {...pageProps} />
